9c859ee9c4abc3ce237177ce72c00b351513570e
ninthcircle <mainlakshayhoon@gmail.com>
2025-12-21 18:52:42 +0530

    Fly me to the moon

---
+++ SETUP.ps1
@@ -0,0 +1,38 @@
+if (-not (Test-Path ".git" -PathType Container)) {
+    throw "ERROR: Not a git repository (.git directory not found)."
+}
+
+$GitHooksDirectory = Join-Path ".git" "hooks"
+New-Item -ItemType Directory -Path $GitHooksDirectory -Force | Out-Null
+
+# Create temp-directory
+$TempDirectory = New-Item -ItemType Directory -Path (
+    Join-Path ([System.IO.Path]::GetTempPath()) ([System.Guid]::NewGuid())
+)
+
+try {
+    # Shallow-Clone repository
+    git clone --depth 1 https://github.com/nos1dot618/git-hook-java.git $TempDirectory
+    if ($LASTEXITCODE -ne 0) {
+        throw "ERROR: git clone failed."
+    }
+
+    # Move .checkstyle (replace if exists)
+    $CheckstyleDestinationDirectory = ".\.checkstyle"
+    if (Test-Path $CheckstyleDestinationDirectory) {
+        Remove-Item $CheckstyleDestinationDirectory -Recurse -Force
+    }
+    Move-Item (Join-Path $TempDirectory ".checkstyle") $CheckstyleDestinationDirectory
+
+    # Move pre-commit hook
+    $PreCommitHookSource = Join-Path $TempDirectory "pre-commit"
+    $PreCommitHookDestination = Join-Path $GitHooksDirectory "pre-commit"
+
+    Move-Item $PreCommitHookSource $PreCommitHookDestination -Force
+}
+finally {
+    # Always cleanup temp-directory
+    if (Test-Path $TempDirectory) {
+        Remove-Item $TempDirectory -Recurse -Force
+    }
+}
